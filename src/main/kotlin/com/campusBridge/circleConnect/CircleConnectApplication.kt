0f725892cb04bb8172ab66ed875e4fbb5474f92b
anthem-landing18 <imokenpi2011@gmail.com>
2023-09-05 02:18:05 +0900

    initial commit

---
+++ src/main/kotlin/com/campusBridge/circleConnect/CircleConnectApplication.kt
@@ -0,0 +1,11 @@
+package com.campusBridge.circleConnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CircleConnectApplication
+
+fun main(args: Array<String>) {
+	runApplication<CircleConnectApplication>(*args)
+}
---
+++ src/test/kotlin/com/campusBridge/circleConnect/CircleConnectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.campusBridge.circleConnect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CircleConnectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
